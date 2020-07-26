4099cc9c89b0993edd7f2717047f1b93ee0e24d2
yasiaan <yzouhri5@gmail.com>
2020-07-26 21:19:45 +0100

    modification professor done

--- src/Controller/ResponsableController.php
+++ src/Controller/ResponsableController.php
@@ -68,10 +68,13 @@ class ResponsableController extends AbstractController
     {
         if($id == null){
             $enseignant = new Professeur();
+            $mail_title = 'Profil ajouté avec succès';
         }else{
             $enseignant = $repo_enseignant->find($id);
-            $user_array = $connection->fetchAll('SELECT * FROM user WHERE professeur_id = 11');
+            $user_array = $connection->fetchAll("SELECT * FROM user WHERE professeur_id = $id");
+            dump($user_array);
             $user = $repo_user->find($user_array[0]['id']);
+            $mail_title = 'Profil modifié avec succès';
         }
         
         $form = $this->createForm(EnseignantType::class, $enseignant);
@@ -92,28 +95,36 @@ class ResponsableController extends AbstractController
             if($id == null){
                 $x = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
                 for($i = 0; $i <= 6; $i++){
-                    $pwd .= $x[rand(0, strlen($x))];
+                    $pwd .= $x[rand(0, strlen($x)-1)];
                 }
             }else{
                 $pwd = $enseignant->password;
             }
-            $hash = $encoder->encodePassword($user, $pwd);
+
+            if($user->getPassword() !== $pwd){
+                $hash = $encoder->encodePassword($user, $pwd);
+                $user->setPassword($hash);
+                $body = strtoupper($enseignant->getNom())." ".$enseignant->getPrenom().",\nVous pouvez vous authentifier en utilisant les informations ci-dessous.\n
+                    E-mail: ".$enseignant->getEmail()."\n
+                    Mot de passe: ".$pwd;
+            }else{
+                $body = strtoupper($enseignant->getNom())." ".$enseignant->getPrenom().",\nVotre profil a été modifié par le responsable avec succès.";
+            }
             $user->setUsername($enseignant->getNom())
                  ->setEmail($enseignant->getEmail())
-                 ->setPassword($hash)
                  ->setProfesseur($enseignant);
             
             $manager->persist($user);
             
             $manager->flush();
+
             
-            $message = (new \Swift_Message('Bienvenue avec nous dans l\'INSEA'))
+            
+            $message = (new \Swift_Message($mail_title))
                 ->setFrom('insea.inscription@gmail.com')
                 ->setTo($enseignant->getEmail())
                 ->setBody(
-                    strtoupper($enseignant->getNom())." ".$enseignant->getPrenom().",\nVous pouvez vous authentifier en utilisant les informations ci-dessous.\n
-                    E-mail: ".$enseignant->getEmail()."\n
-                    Mot de passe: ".$pwd,
+                    $body,
                     'text/plain'
                 );
             $mailer->send($message);
@@ -125,15 +136,33 @@ class ResponsableController extends AbstractController
             return $this->render('responsable/enseignant_ajout.html.twig', [
                 'form_enseignant' => $form->createView(),
                 'pwd' => 0,
-                'title' => 'L\'ajout d\'un enseignat'
+                'title' => 'L\'ajout d\'un enseignat',
+                'button' => 'Ajouter'
             ]);
         }else{
             return $this->render('responsable/enseignant_ajout.html.twig', [
                 'form_enseignant' => $form->createView(),
                 'pwd' => 1,
                 'title' => "La modification des informations de :\n".$enseignant->getNom()." ".$enseignant->getPrenom(),
-                'password' => $user_array[0]['password']
+                'password' => $user_array[0]['password'],
+                'button' => 'Modifier'
             ]);
         }
+    }
+
+    /**
+     * @Route("/responsable/enseignant/suppression/{id}", name="responsable_suppression_enseignant")
+     */
+    public function delete_enseignant(Connection $connection, $id, ProfesseurRepository $repo_enseignant, UserRepository $repo_user, EntityManagerInterface $manager)
+    {
+        $enseignant = $repo_enseignant->find($id);
+        $user_array = $connection->fetchAll("SELECT * FROM user WHERE professeur_id = $id");
+        $user = $repo_user->find($user_array[0]['id']);
+
+        $manager->remove($user);
+        $manager->remove($enseignant);
+        $manager->flush();
+
+        return $this->redirectToRoute('responsable_enseignant');
     }
 }
